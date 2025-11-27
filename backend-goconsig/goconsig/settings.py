3ea586ca9dfbce13b1472b1edea5b65c4132a571
João Manoel <157231849+NegativoPai@users.noreply.github.com>
2025-11-27 20:40:24 -0300

    Removendo entrada duplicada de 'app_bancos' nos settings

--- backend-goconsig/goconsig/settings.py
+++ backend-goconsig/goconsig/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     #=====================================================
-    'app_bancos',
     'app_parceiros',
     'app_usuarios',
     'app_bancos',
